8d2fe4cf18aead06ae7a9072c61d5920c45193e8
Luke Williams <shmookey@shmookey.net>
2014-06-14 12:12:16 +0000

    Fix local instance URL.

--- gaff/conf.py
+++ gaff/conf.py
@@ -1,5 +1,5 @@
 '''conf.py -- Gaff configuration'''
 
-API_URI = 'http://local.gaff.shmookey.net/wiki/api.php'
+API_URI = 'http://gaff.shmookey.net/wiki/api.php'
 API_USERNAME = 'compilebot'
 API_PASSWORD = 'DgfoxiSF3s03'
